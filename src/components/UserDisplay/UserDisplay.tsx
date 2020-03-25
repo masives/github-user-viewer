fecb7f75ac9d1b05aabd0af0c64a7c054e6bdffd
Jozef Piecyk <jozollo@gmail.com>
2020-03-25 14:01:39 +0100

    add userDisplay styling for container, image and list

--- src/components/UserDisplay/UserDisplay.tsx
+++ src/components/UserDisplay/UserDisplay.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { GithubRepository, GithubUser } from '../../types/github';
 import LoadingIndicator from '../LoadingIndicator';
+import styles from './UserDisplay.module.css';
 
 type PropTypes = {
   repositories: GithubRepository[];
@@ -9,9 +10,6 @@ type PropTypes = {
 };
 
 const UserDisplay = ({ repositories, userDetails, isLoading }: PropTypes) => {
-  if (!userDetails) {
-    return null;
-  }
   if (isLoading) {
     return <LoadingIndicator />;
   }
@@ -22,17 +20,19 @@ const UserDisplay = ({ repositories, userDetails, isLoading }: PropTypes) => {
   const { name, avatar_url, bio } = userDetails;
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>{name}</h1>
-      <img src={avatar_url} alt={`avatar of ${name}`} />
+      <img className={styles.profileImage} src={avatar_url} alt={`avatar of ${name}`} />
       <p>{Boolean(bio) ? bio : 'User has no bio available'}</p>
       <div>
         <h2>Top repositories</h2>
-        <ol>
+        <ol className={styles.repositoryList}>
           {repositories.map((repository) => {
             return (
               <li key={repository.id}>
-                <a href={repository.html_url}>{repository.name}</a>
+                <a href={repository.html_url} target="_blank" rel="noopener noreferrer">
+                  {repository.name}
+                </a>
               </li>
             );
           })}
